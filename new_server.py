3e41e70c6c3b60a1360a46de84f3541ccc334d42
Robert Gravelle <113545882+rgravelle-prog-1@users.noreply.github.com>
2023-10-11 01:59:34 -0400

    Add files via upload

--- new_server.py
+++ new_server.py
@@ -4,6 +4,11 @@ from EmotionDetection.emotion_detection import emotion_predictor
 app = Flask("Emotion Analyzer")
 
 
+def compare_scores(score):
+    # Custom comparison function to treat None as a very small value
+    return score if score is not None else float('-inf')
+
+
 @app.route("/emotionDetector")
 def sent_analyzer():
     text_to_analyze = request.args.get('textToAnalyze')
@@ -13,20 +18,21 @@ def sent_analyzer():
         print(f"Invalid response format from emotion predictor: {response}")
         return "Invalid response format from emotion predictor"
 
-    anger_score = response.get('anger', 0)
-    disgust_score = response.get('disgust', 0)
-    fear_score = response.get('fear', 0)
-    joy_score = response.get('joy', 0)
-    sadness_score = response.get('sadness', 0)
+    anger_score = response.get('anger', None)
+    disgust_score = response.get('disgust', None)
+    fear_score = response.get('fear', None)
+    joy_score = response.get('joy', None)
+    sadness_score = response.get('sadness', None)
 
     emotion_score = {
-        'anger': anger_score,
-        'disgust': disgust_score,
-        'fear': fear_score,
-        'joy': joy_score,
-        'sadness': sadness_score
+        'anger': compare_scores(anger_score),
+        'disgust': compare_scores(disgust_score),
+        'fear': compare_scores(fear_score),
+        'joy': compare_scores(joy_score),
+        'sadness': compare_scores(sadness_score)
     }
 
+    # Use custom comparison function
     dominant = max(emotion_score, key=emotion_score.get)
     if dominant is None:
         return "Invalid text! Please try again."
