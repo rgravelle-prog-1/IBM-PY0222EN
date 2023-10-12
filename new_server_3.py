5e099dc9c13b4c65d77ec25ee2a381b3617f9b4f
Robert Gravelle <113545882+rgravelle-prog-1@users.noreply.github.com>
2023-10-12 01:11:34 -0400

    Add files via upload

--- new_server_3.py
+++ new_server_3.py
@@ -1,15 +1,32 @@
+"""
+This is a Flask application for emotion analysis.
+
+Author: Robert Gravelle
+Date: Ongoing
+"""
 import json
+import logging
 import requests
+
 from flask import Flask, render_template, request
+
+# Configure the logging level as needed for pylint I set it to info
+logging.basicConfig(level=logging.INFO)
 
 app = Flask("Emotion Analyzer")
 
 
 def compare_scores(score):
+    """
+    Compare scores and return the score if not None, else return negative infinity.
+    """
     return score if score is not None else float('-inf')
 
 
 def emotion_detector(text_to_analyze):
+    """
+    This function performs emotion detection .
+    """
     if not text_to_analyze.strip():
         return {
             'anger': None,
@@ -20,12 +37,13 @@ def emotion_detector(text_to_analyze):
             'dominant_emotion': None
         }
 
-    url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
+    url = ('https://sn-watson-emotion.labs.skills.network/'
+           'v1/watson.runtime.nlp.v1/NlpService/EmotionPredict')
     header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     myobj = {"raw_document": {"text": text_to_analyze}}
 
     try:
-        response = requests.post(url, json=myobj, headers=header)
+        response = requests.post(url, json=myobj, headers=header, timeout=10)
         response.raise_for_status()
     except requests.RequestException as e:
         print(f"Request error: {e}")
@@ -59,11 +77,16 @@ def emotion_detector(text_to_analyze):
 
 @app.route("/emotionDetector")
 def sent_analyzer():
+    """
+    Analyzes the input text for emotion.
+
+    Returns: formatted string containing emotion analysis results.
+    """
     text_to_analyze = request.args.get('textToAnalyze')
     response = emotion_detector(text_to_analyze)
 
     if not isinstance(response, dict):
-        app.logger.error(f"Invalid response format from emotion predictor: {response}")
+        logging.info("Invalid response format from emotion predictor: %s", response)
         return "Invalid response format from emotion predictor"
 
     status_code = response.get('status_code', None)
@@ -95,10 +118,15 @@ def sent_analyzer():
     dominant = max(emotion_score, key=emotion_score.get)
 
     if dominant is None:
+        logging.info("Invalid text! Please try again.")
         return "Invalid text! Please try again."
 
-    string1 = f"Your statement is considered 'anger':"
-    string2 = f"{anger_score}, 'disgust' : {disgust_score}, 'fear': {fear_score}, 'joy': {joy_score}"
+    string1 = "Your statement is considered 'anger':"
+    string2 = (
+        f"{anger_score}, 'disgust' : {disgust_score}, "
+        f"'fear': {fear_score}, 'joy': {joy_score}"
+    )
+
     string3 = f"and 'sadness': {sadness_score}. The dominant emotion is {dominant}"
 
     return f"{string1} {string2} {string3}"
@@ -106,6 +134,11 @@ def sent_analyzer():
 
 @app.route("/")
 def render_index_page():
+    """
+    Renders the index.html page.
+
+    Returns:Flask render_template in rendered HTML template.
+    """
     return render_template('index.html')
 
 
